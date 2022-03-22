48a17fa6f6d9f4a61dc48276c796787ba326f4f9
chenken <chenken@huohua.cn>
2022-03-22 15:04:17 +0800

    add mocha for test

--- src/binarySearch.ts
+++ src/binarySearch.ts
@@ -1,23 +1,21 @@
-export class BinarySearch {
-    public binarySearch(list, item) {
-        let low = 0;
-        let high = list.length - 1;
+export function binarySearch(list, item) {
+    let low = 0;
+    let high = list.length - 1;
 
-        while (low <= high) {
-            const mid = Math.floor((low + high) / 2);
-            const guess = list[mid];
+    while (low <= high) {
+        const mid = Math.floor((low + high) / 2);
+        const guess = list[mid];
 
-            if (guess === item) {
-                return mid;
-            }
-
-            if (guess > item) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+        if (guess === item) {
+            return mid;
         }
 
-        return null; //if not found
+        if (guess > item) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
     }
+
+    return null; //if not found
 }
