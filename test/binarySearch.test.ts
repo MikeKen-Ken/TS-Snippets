f04390827c6d39f67f781788e2d078dbcbf2dd1c
chenken <chenken@huohua.cn>
2022-03-22 21:10:53 +0800

    fix test title

--- test/binarySearch.test.ts
+++ test/binarySearch.test.ts
@@ -1,13 +1,13 @@
-import { binarySearch } from './../src/binarySearch';
+import { binarySearch } from '../src/binarySearch';
 import { assert } from 'chai';
 import { it } from 'mocha';
 
-describe('avl test', function () {
+describe('Binary Search', function () {
     const arr = [-10, -3, -1, 0, 4, 6, 7, 12, 354, 1245];
-    it('build avl', function () {
+    it('at the start index', function () {
         assert.equal(binarySearch(arr, -10), 0);
     });
-    it('at end index', function () {
+    it('at the end index', function () {
         assert.equal(binarySearch(arr, 1245), 9);
     });
     it('find zero', function () {
